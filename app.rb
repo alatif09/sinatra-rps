7eed98d9543d02713ea6b66be4b96a3f67252537
alatif09 <146213608+alatif09@users.noreply.github.com>
2023-10-24 03:45:44 +0000

    complete

--- app.rb
+++ app.rb
@@ -1,9 +1,68 @@
 require "sinatra"
 require "sinatra/reloader"
 
+computer_choices = ["rock", "paper", "scissors"]
+
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:elephant)
 end
+
+get("/rock") do
+  
+  computer_choice = computer_choices.sample
+  @comp_choice = []
+  @result = []
+
+  if computer_choice == "rock"
+    @comp_choice.push("They played #{computer_choice}!")
+    @result.push("We tied!")
+  elsif computer_choice == "scissors"
+    @comp_choice.push("They played #{computer_choice}!")
+    @result.push("We won!")
+  elsif computer_choice == "paper"
+    @comp_choice.push("They played #{computer_choice}!")
+    @result.push("We lost!")   
+  end
+
+ erb(:rock)
+end
+
+get("/paper") do
+  
+  computer_choice = computer_choices.sample
+  @comp_choice = []
+  @result = []
+
+  if computer_choice == "rock"
+    @comp_choice.push("They played #{computer_choice}!")
+    @result.push("We won!")
+  elsif computer_choice == "scissors"
+    @comp_choice.push("They played #{computer_choice}!")
+    @result.push("We lost!")
+  elsif computer_choice == "paper"
+    @comp_choice.push("They played #{computer_choice}!")
+    @result.push("We tied!")   
+  end
+   
+ erb(:paper)
+end 
+
+get("/scissors") do
+  
+  computer_choice = computer_choices.sample
+  @comp_choice = []
+  @result = []
+
+  if computer_choice == "rock"
+    @comp_choice.push("They played #{computer_choice}!")
+    @result.push("We lost!")
+  elsif computer_choice == "scissors"
+    @comp_choice.push("They played #{computer_choice}!")
+    @result.push("We tied!")
+  elsif computer_choice == "paper"
+    @comp_choice.push("They played #{computer_choice}!")
+    @result.push("We won!")   
+  end
+   
+ erb(:scissors)
+end 
